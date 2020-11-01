04d4266bfa26afa6ef865c5efd71cdb8cc7249ba
Rohit Rajat Chattopadhyay <rohit.chattopadhyay1@gmail.com>
2020-11-02 00:51:06 +0530

    Add netlify settings

--- src/components/chatpreview/index.js
+++ src/components/chatpreview/index.js
@@ -1,14 +1,13 @@
 import { h } from 'preact';
-import { Link } from 'preact-router/match';
 
 import style from "./style.css"
 
 const Preview = ({ user_id, user_name }) => (
     <div class="border-bottom">
-        <Link href={`/chat/${user_id}`} class={`lead py-1 w-100 col-12 ${style.previewLink}`} activeClassName="text-dark bg-light">
+        <a href={`/chat/${user_id}`} class={`lead py-1 w-100 col-12 ${style.previewLink}`} activeClassName="text-dark bg-light">
             {user_name}
             {/* <span class="text-right w-25 d-inline-block"><span class="text-success">●</span></span> */}
-        </Link>
+        </a>
     </div>
 );
 
